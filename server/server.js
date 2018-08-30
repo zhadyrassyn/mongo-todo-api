9109865e734555da968eacb2053bfa2567d7b99c
daniyar <zhadyrassyn.daniyar@is.sdu.edu.kz>
2018-08-30 15:10:05 +0600

    setup app for heroku

--- server/server.js
+++ server/server.js
@@ -50,8 +50,9 @@ app.get('/todos/:id', (req, res) => {
 
 });
 
-app.listen(3000, () => {
-  console.log('started on port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`started on port ${port}`);
 });
 
 module.exports = {app}
