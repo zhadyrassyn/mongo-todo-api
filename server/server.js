f48c61e76de7ee507fca8eee11d6b79e5fb8af69
daniyar <zhadyrassyn.daniyar@is.sdu.edu.kz>
2018-08-30 16:56:44 +0600

    add PATCH /todos/:id ROUTE

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb')
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb')
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
 
-var app = express();
+const app = express();
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
@@ -48,7 +49,7 @@ app.get('/todos/:id', (req, res) => {
 
 });
 
-// DETE /todos/:id
+// DELETE /todos/:id
 app.delete('/todos/:id', (req, res) => {
   const id = req.params.id
   if(!ObjectID.isValid(id)) {
@@ -64,6 +65,31 @@ app.delete('/todos/:id', (req, res) => {
   }).catch(err => {
     res.status(400).send();
   })
+});
+
+app.patch('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  const body = _.pick(req.body, ['text', 'completed']);
+  console.log('body ', body);
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completed = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then(todo => {
+    if(!todo) {
+      return res.status(404).send(todo);
+    }
+
+    res.send({todo})
+  }).catch(e => res.status(400).send(e));
 });
 
 const port = process.env.PORT || 3000;
